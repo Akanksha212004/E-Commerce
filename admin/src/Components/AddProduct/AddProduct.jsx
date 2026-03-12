27b06465c83d3977b34506ee06be47fad12292b0
Akanksha Yadav <akankshayadav212004@gmail.com>
2026-03-12 23:35:48 +0530

    connect frontend to deployed backend

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -29,7 +29,7 @@ const AddProduct = () => {
         let formData = new FormData();
         formData.append('product', image);
 
-        await fetch('http://localhost:4000/upload',{
+        await fetch('http://e-commerce-backend-ac08.onrender.com/upload',{
             method:'POST',
             headers:{
                 Accept:'application/json',
@@ -41,7 +41,7 @@ const AddProduct = () => {
         {
             product.image = responseData.image_url;
             console.log(product);
-            await fetch('http://localhost:4000/addproduct',{
+            await fetch('http://e-commerce-backend-ac08.onrender.com/addproduct',{
                 method:"POST",
                 headers:{
                     Accept:'application/json',
