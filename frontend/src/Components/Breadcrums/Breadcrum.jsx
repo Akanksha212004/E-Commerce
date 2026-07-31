a4c672c3e7812730fd25c5f9f45d09b8cb030ec8
Akanksha Yadav <akankshayadav212004@gmail.com>
2026-07-31 22:35:46 +0530

    fix unused import eslint error

--- frontend/src/Components/Breadcrums/Breadcrum.jsx
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
-import './Breadcrum.css';
-import arrow_icon from '../Assets/breadcrum_arrow.png';
+// import React from 'react';
+// import './Breadcrum.css';
+// import arrow_icon from '../Assets/breadcrum_arrow.png';
 
-// const Breadcrum = (props) => {
-//     const {product} = props;
+// // const Breadcrum = (props) => {
+// //     const {product} = props;
+// //   return (
+// //     <div className='breadcrum'>
+// //         HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
+// //     </div>
+// //   )
+// // }
+
+
+// const Breadcrum = ({product}) => {
+//   if (!product) return null;  // ya <div>Loading...</div>
+
 //   return (
 //     <div className='breadcrum'>
-//         HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
+//       HOME / {product.category} / {product.name}
 //     </div>
 //   )
 // }
 
+// export default Breadcrum;
+
+
+
+
+
+import React from 'react';
+import './Breadcrum.css';
+import arrow_icon from '../Assets/breadcrum_arrow.png';
 
 const Breadcrum = ({product}) => {
-  if (!product) return null;  // ya <div>Loading...</div>
+  if (!product) return null;
 
   return (
     <div className='breadcrum'>
-      HOME / {product.category} / {product.name}
+      HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
     </div>
   )
 }
